f45b2e808243174d8e31d9db3f8932dbd3481697
Phil Sergi <phil.sergi@gmail.com>
2009-07-02 17:07:27 -0400

    EarthTools.get method

--- lib/earth_tools.rb
+++ lib/earth_tools.rb
@@ -1,6 +1,7 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+require 'open-uri'
 require 'nokogiri'
 
 require 'earth_tools/xml/element'
@@ -17,9 +18,22 @@ module EarthTools
   VERSION = '0.0.1'
 
   class << self
-    attr_accessor :version, :base_uri
+    attr_accessor :base_uri
   end
 
   self.base_uri = 'http://www.earthtools.org'
+
+  def self.get(service, version = nil, *params)
+    service = versioned_service(service, version)
+    response = open(File.join(self.base_uri, service, *params))
+    EarthTools::XML::Element.new(Nokogiri::XML(response))
+  end
+
+  private
+
+  def self.versioned_service(service, version)
+    return service if version.nil?
+    "#{service}-#{version}"
+  end
 
 end
--- spec/earth_tools_spec.rb
+++ spec/earth_tools_spec.rb
@@ -1,15 +1,44 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper.rb')
 
 describe EarthTools do
-  it "should have a version accessor" do
-    EarthTools.should respond_to(:version)
-    EarthTools.should respond_to(:version=)
-  end
   it "should have a base uri accessor" do
     EarthTools.should respond_to(:base_uri)
     EarthTools.should respond_to(:base_uri=)
   end
   it "should set the base_uri to 'http://www.earthtools.org" do
     EarthTools.base_uri.should == 'http://www.earthtools.org'
+  end
+
+  describe ".get" do
+    before do
+      Nokogiri.stub!(:XML)
+      @response = '<?xml version="1.0" encoding="ISO-8859-1" ?><root></root>'
+      EarthTools.stub!(:open).and_return(@response)
+    end
+    it "should make a request with the base uri" do
+      EarthTools.should_receive(:open).with(Regexp.new(Regexp.escape(EarthTools.base_uri)))
+      EarthTools.get('timezone')
+    end
+    it "should make a request to the service passed" do
+      EarthTools.should_receive(:open).with(File.join(EarthTools.base_uri, "timezone"))
+      EarthTools.get('timezone')
+    end
+    it "should make a versioned request if the version is passed" do
+      EarthTools.should_receive(:open).with(File.join(EarthTools.base_uri, "timezone-1.1"))
+      EarthTools.get('timezone', '1.1')
+    end
+    it "should seperate each of the params with slashes" do
+      EarthTools.should_receive(:open).with(File.join(EarthTools.base_uri, "timezone", "one", "two", "three"))
+      EarthTools.get('timezone', nil, 'one', 'two', 'three')
+    end
+    it "should build a Nokogiri::XML out of the response" do
+      Nokogiri.should_receive(:XML).with(@response)
+      EarthTools.get('timezone')
+    end
+    it "should return a EarthTools::XML::Element build from the Nokogiri document" do
+      Nokogiri.stub!(:XML).and_return('xml')
+      EarthTools::XML::Element.should_receive(:new).with('xml').and_return('earth tools element')
+      EarthTools.get('timezone').should == 'earth tools element'
+    end
   end
 end
